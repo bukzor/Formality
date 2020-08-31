a6b06c77d6816114bbacc228e470e4badcfa4b68
Buck Evan <buck.2019@gmail.com>
2020-08-30 20:54:28 -0700

    fmc core stringify

--- python3/formality/core/token.py
+++ python3/formality/core/token.py
@@ -1,18 +1,102 @@
 """Formality Core."""
+# Translations from the javascript:
+#  * adhoc objects replaced with namedtuple classes
+#  * functions that switch on the constructor replaced with methods
 from collections import namedtuple
 
-Var = namedtuple("Var", "indx")
-Ref = namedtuple("Ref", "name")
-Typ = namedtuple("Typ", "")
-All = namedtuple("All", "eras self name bind body")
-Lam = namedtuple("Lam", "eras name body")
-App = namedtuple("App", "eras func argm")
-Let = namedtuple("Let", "name expr body")
-Ann = namedtuple("Ann", "done expr type")
-Loc = namedtuple("Loc", "frum upto expr")
+Term = namedtuple
+List = namedtuple
+del namedtuple
 
 
-class Nil(tuple):
+class Var(Term("Var", "indx")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        del depth
+        return self.indx.split("#")[0]
+
+
+class Ref(Term("Ref", "name")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        del depth
+        return self.name
+
+
+class Typ(Term("Typ", "")):
+    __slots__ = ()
+
+    @staticmethod
+    def stringify(depth=0):
+        del depth
+        return "*"
+
+
+class All(Term("All", "eras self name bind body")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        bind = "Ɐ" if self.eras else "Π"
+        slf_ = self.self or ("x" + str(depth + 0))
+        name = self.name or ("x" + str(depth + 1))
+        type = self.bind.stringify(depth)
+        body = self.body(Var(slf_ + "#"), Var(name + "#")).stringify(depth + 2)
+        return f"{bind}{slf_}({name}:{type}) {body}"
+
+
+class Lam(Term("Lam", "eras name body")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        bind = "Λ" if self.eras else "λ"
+        name = self.name or ("x" + str(depth))
+        body = self.body(Var(name + "#")).stringify(depth)
+        return f"{bind}{name} {body}"
+
+
+class App(Term("App", "eras func argm")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        open = "<" if self.eras else "("
+        func = self.func.stringify(depth)
+        argm = self.argm.stringify(depth)
+        clos = ">" if self.eras else ")"
+        return f"{open}{func} {argm}{clos}"
+
+
+class Let(Term("Let", "name expr body")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        name = self.name or ("x" + str(depth))
+        expr = self.expr.stringify(depth)
+        body = self.body(Var(name + "#")).stringify(depth + 1)
+        return f"${name}={expr};{body}"
+
+
+class Ann(Term("Ann", "done expr type")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        type = self.type.stringify(depth)
+        expr = self.expr.stringify(depth)
+        return f":{type} {expr}"
+
+
+class Loc(Term("Loc", "frum upto expr")):
+    __slots__ = ()
+
+    def stringify(self, depth=0):
+        return self.expr.stringify(depth)
+
+
+del Term
+
+
+class Nil(List("Nil", "")):
     __slots__ = ()
     size = 0
 
@@ -24,7 +108,7 @@ class Nil(tuple):
         del cond, indx
 
 
-class Ext(namedtuple("Ext", "head tail size")):
+class Ext(List("Ext", "head tail size")):
     __slots__ = ()
 
     def __new__(cls, head, tail):
@@ -37,4 +121,27 @@ class Ext(namedtuple("Ext", "head tail size")):
             return self.tail.find(cond, indx + 1)
 
 
-del namedtuple
+del List
+
+
+__all__ = (
+    "Var",
+    "Ref",
+    "Typ",
+    "All",
+    "Lam",
+    "App",
+    "Let",
+    "Ann",
+    "Loc",
+    "Ext",
+    "Nil",
+    "parse",
+    "reduce",
+    "normalize",
+    "Err",
+    "typeinfer",
+    "typecheck",
+    "typesynth",
+    "equal",
+)
